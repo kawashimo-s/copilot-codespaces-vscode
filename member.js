65056be458e914a9d16e52ed98d371101737b1b7
Shimokawa Shion <90030892+kawashimo-s@users.noreply.github.com>
2025-12-06 02:49:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        scope: {
+            member: '='
+        },
+        templateUrl: 'templates/skills-member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMemberCtrl'
+    };
+}   
+
+module.exports = {
+    skillsMember
+};
